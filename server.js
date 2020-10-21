ff29248c7fae7b216873437f91ba24c03d50edde
Cameron Lay <cameronlay15@gmail.com>
2020-10-21 16:39:44 -0400

    joining

--- server.js
+++ server.js
@@ -10,3 +10,17 @@ const app = express();
 const path = require("path");
 
 app.use(logger("dev"));
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(`${__dirname}/public/exercise.html`));
+  });
+  
+  app.get("/stats", (req, res) => {
+    res.sendFile(path.join(`${__dirname}/public/stats.html`));
+  });
+  
+  
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(express.static("public"));
+  require("./routes/routes.js")(app);
