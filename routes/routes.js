7b2a8650d9a808baad2520d881d57574dca2d46e
Cameron Lay <cameronlay15@gmail.com>
2020-10-21 17:00:17 -0400

    const data

--- routes/routes.js
+++ routes/routes.js
@@ -26,3 +26,55 @@ module.exports = (app) => {
       day: {
         $exists: true,
       },
+    }, (err, docs) => {
+        const data = [
+          {
+            dayOfWeek: "Monday",
+            dayNumber: 1,
+            totalDuration: 0,
+            totalWeight: 0,
+            exerciseNames: [],
+          },
+          {
+            dayOfWeek: "Tuesday",
+            dayNumber: 2,
+            totalDuration: 0,
+            totalWeight: 0,
+            exerciseNames: [],
+          },
+          {
+            dayOfWeek: "Wednesday",
+            dayNumber: 3,
+            totalDuration: 0,
+            totalWeight: 0,
+            exerciseNames: [],
+          },
+          {
+            dayOfWeek: "Thursday",
+            dayNumber: 4,
+            totalDuration: 0,
+            totalWeight: 0,
+            exerciseNames: [],
+          },
+          {
+            dayOfWeek: "Friday",
+            dayNumber: 5,
+            totalDuration: 0,
+            totalWeight: 0,
+            exerciseNames: [],
+          },
+          {
+            dayOfWeek: "Saturday",
+            dayNumber: 6,
+            totalDuration: 0,
+            totalWeight: 0,
+            exerciseNames: [],
+          },
+          {
+            dayOfWeek: "Sunday",
+            dayNumber: 7,
+            totalDuration: 0,
+            totalWeight: 0,
+            exerciseNames: [],
+          },
+        ];
